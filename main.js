63551310d3e58b8415e30016bc7d24801c0a5d2e
Keith Walker <Keithrwalker418@gmail.com>
2019-03-16 15:50:45 -0500

    Flexbox is set, and ready

--- main.js
+++ main.js
@@ -225,13 +225,13 @@ const arrayLoop = (yourArray) => {// LOOPS THROUGH AN ARRAY | ADDS HTML TO A VAR
     let printString = '';
 
     yourArray.forEach ((pet) => {
-        printString += `<div class="card">`;
-        printString +=      `<h2>${pet.name}</h2>`;
-        printString +=      `<img src="${pet.imageUrl}" alt="photo of ${pet.name} who is a ${pet.type}">`;
-        printString +=      `<p>Color: ${pet.color}</p>`;
-        printString +=      `<p>Skill: ${pet.specialSkill}</p>`;
-        printString +=      `<h6>Type: ${pet.type}</h6>`;
-        printString += `</div>`;
+      printString +=  `<div class="card">`;
+      printString +=        `<h2>${pet.name}</h2>`;
+      printString +=      `<img src="${pet.imageUrl}" alt="photo of ${pet.name} who is a ${pet.type}">`;
+      printString +=      `<p>Color: ${pet.color}</p>`;
+      printString +=      `<p>Skill: ${pet.specialSkill}</p>`;
+      printString +=      `<h6 class="${pet.type}Class">Type: ${pet.type}</h6>`;
+      printString +=  `</div>`;
     })
     print('testDiv', printString);
 };
